2779bc7f6d8597b05c2448b539a2203774779fff
Alisha <alisagrg102@gmail.com>
2025-06-07 09:49:40 +0900

    Add transaction to list dynamically

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById("form").addEventListener("submit", function(e) {
   e.preventDefault();
   alert("Transaction added!");
 });
+const list = document.getElementById("list");
+...
+const li = document.createElement("li");
+li.innerText = `${text}: $${amount}`;
+list.appendChild(li);
